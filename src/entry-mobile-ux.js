c5176d56813935ffe2436dd7d80d5b386c65d769
OST DA <organization@dealinvests.com>
2026-08-07 20:17:52 +0700

    Add files via upload

--- src/entry-mobile-ux.js
+++ src/entry-mobile-ux.js
@@ -4,7 +4,7 @@ import { enhanceMobileWebResponse } from "./mobile-web-ux.js";
 
 export { MultiExpenseSession } from "./index.js";
 
-const VERSION = "DEAL_LINE_BOT_v4.4_CLOSE_WITH_X_GUIDE_20260807";
+const VERSION = "DEAL_LINE_BOT_v4.5_BLACK_CARD_CLOSE_X_ACTIVE_20260807";
 
 async function wrappedFetch(request, env, ctx) {
   const url = new URL(request.url);
